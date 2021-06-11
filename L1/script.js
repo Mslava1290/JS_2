f3fb465677360e1dd47310945de1a9aec95dd1ef
mslava1290 <mslava1290@gmail.com>
2021-06-11 14:28:46 +0300

    мелкие правки

--- L1/script.js
+++ L1/script.js
@@ -1,31 +1,41 @@
 const goods = [{
-        title: 'Shirt',
-        price: 150
+        id: 1,
+        title: 'Notebook',
+        price: 2000
     },
     {
-        title: 'Socks',
+        id: 2,
+        title: 'Mouse',
+        price: 20
+    },
+    {
+        id: 3,
+        title: 'Keyboard',
+        price: 200
+    },
+    {
+        id: 4,
+        title: 'Gamepad',
         price: 50
-    },
-    {
-        title: 'Jacket',
-        price: 350
-    },
-    {
-        title: 'Shoes',
-        price: 250
     },
 ];
 
-const renderGoodsItem = (title = 'null', price = '0') => {
+const renderGoodsItem = x => {
     let img = '<img src="http://placehold.it/150x100/"/>';
     let buybutton = '<button class="buy-button" type="button">Купить</button>';
 
-    return `<div class="goods-item">${img}<h3>${title}</h3><p>${price}</p> ${buybutton}</div>`;
+    return `<div class="goods-item">
+                ${img}
+                <h3>${x.title}</h3>
+                <p>${x.price}</p>
+                ${buybutton}
+            </div>`;
 };
 
 
 const renderGoodsList = (list = []) => {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
+    let goodsList = list.map(item => renderGoodsItem(item));
+    console.log(goodsList);
     document.querySelector('.goods-list').innerHTML = goodsList.join('');
 }
 renderGoodsList(goods);
