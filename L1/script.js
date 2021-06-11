0366af0febbc64372392e19bebdae41567356940
mslava1290 <mslava1290@gmail.com>
2021-06-11 14:08:31 +0300

    Правки стиля

--- L1/script.js
+++ L1/script.js
@@ -1,14 +1,25 @@
-const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
-    ];
+const goods = [{
+        title: 'Shirt',
+        price: 150
+    },
+    {
+        title: 'Socks',
+        price: 50
+    },
+    {
+        title: 'Jacket',
+        price: 350
+    },
+    {
+        title: 'Shoes',
+        price: 250
+    },
+];
 
 const renderGoodsItem = (title = 'null', price = '0') => {
     let img = '<img src="http://placehold.it/150x100/"/>';
     let buybutton = '<button class="buy-button" type="button">Купить</button>';
-    
+
     return `<div class="goods-item">${img}<h3>${title}</h3><p>${price}</p> ${buybutton}</div>`;
 };
 
