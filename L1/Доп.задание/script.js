4f55fbb57d3df8b978eabb075b1150f38869dd3b
mslava1290 <mslava1290@gmail.com>
2021-06-17 21:03:08 +0300

    Сверстал меню

---
+++ L1/Доп.задание/script.js
@@ -0,0 +1,29 @@
+class Burger {
+    constructor (size, topping, optional_topping) {
+        this.size = size;
+        this.topping = topping;
+        this.optional_topping = optional_topping;
+    }
+    
+    
+    addTopping(topping) {
+    }
+    
+    removeTopping(topping) {
+    }
+    
+    getToppings(topping) {   
+    }
+    
+    getSize() {
+    }
+    
+    getStuffing() {
+    }
+    
+    calculate_price() {
+    }
+    
+    calculateCalories() {
+    }
+}
