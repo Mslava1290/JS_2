3f326108f5cfe24a8702f61f0f51a4c2bf0a7c34
mslava1290 <mslava1290@gmail.com>
2021-06-17 18:51:34 +0300

    Решение ДЗ у уроку 2

--- L1/script.js
+++ L1/script.js
@@ -1,41 +1,96 @@
-const goods = [{
-        id: 1,
-        title: 'Notebook',
-        price: 2000
-    },
-    {
-        id: 2,
-        title: 'Mouse',
-        price: 20
-    },
-    {
-        id: 3,
-        title: 'Keyboard',
-        price: 200
-    },
-    {
-        id: 4,
-        title: 'Gamepad',
-        price: 50
-    },
-];
+/*
+1. Добавьте пустые классы для корзины товаров и элемента корзины товаров. Продумайте, какие методы понадобятся для работы с этими сущностями.
 
-const renderGoodsItem = x => {
-    let img = '<img src="http://placehold.it/150x100/"/>';
-    let buybutton = '<button class="buy-button" type="button">Купить</button>';
+2. Добавьте для GoodsList метод, определяющий суммарную стоимость всех товаров.
 
-    return `<div class="goods-item">
-                ${img}
-                <h3>${x.title}</h3>
-                <p>${x.price}</p>
-                ${buybutton}
-            </div>`;
-};
+3. *Некая сеть фастфуда предлагает несколько видов гамбургеров:
+### Маленький (50 рублей, 20 калорий).
+### Большой (100 рублей, 40 калорий). ### Гамбургер может быть с одним из нескольких видов начинок (обязательно):
+### С сыром (+10 рублей, +20 калорий).
+### С салатом (+20 рублей, +5 калорий).
+### С картофелем (+15 рублей, +10 калорий). 
+### Дополнительно гамбургер можно посыпать приправой (+15 рублей, +0 калорий) и полить майонезом (+20 рублей, +5 калорий). 
 
+### 3Напишите программу, рассчитывающую стоимость и калорийность гамбургера. Можно использовать примерную архитектуру класса из методички, но можно использовать и свою.
+*/
 
-const renderGoodsList = (list = []) => {
-    let goodsList = list.map(item => renderGoodsItem(item));
-    console.log(goodsList);
-    document.querySelector('.goods-list').innerHTML = goodsList.join('');
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();
+        this.render(); //вывод товаров на страницу
+    }
+    _fetchProducts(){
+        this.goods = [
+            {id:1 ,title:'Notebook', price:2000},
+            {id:2 ,title:'Mouse', price:20},
+            {id:3 ,title:'Keyboard', price:200},
+            {id:4 ,title:'Gamepad', price:50},
+        ];
+    }
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const item = new ProductItem(product);
+            block.insertAdjacentHTML('beforeend', item.render());
+        }
+    }
 }
-renderGoodsList(goods);
+class ProductItem {
+    constructor(product, img='http://placehold.it/200x100/'){
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.img = img;
+    }
+    render() {
+        return `<div class="goods-item">
+                    <img class="goods-img" src="${this.img}"/>
+                    <h3>${this.title}</h3>
+                    <p>${this.price}</p>
+                    <button class="buy-button" type="button">Купить</button>
+                </div>`;
+    }
+}
+
+//Класс-заготовка для корзины
+class Bucket {
+    //Метод для оформления заказа
+    Checkout(title, price){
+        this.title = title;
+        this.price = price;
+    };
+    //Метод для быстрой покупки без регистрации
+    CheckoutQuick(title, price){
+        this.title = title;
+        this.price = price;
+    };
+}
+
+//Класс-заготовка для элемента корзины
+class BucketEl {
+    //метод для быстрого изменения количества товара в корзине. Например "+" "-" напротив товара
+    ChangeQuantity(){
+
+    };
+    //метод для удаления товара из корзины
+    DeleteItem(){
+
+    };
+    //метод для быстрого сравнения товара с похожими товарами(другого цвета, комлпектации и тд)
+    GetCompare(){
+
+    };
+    //метод для подсчета суммы в рамках одного товара. Например, если товара берется >1 шт.
+    GetAmount(){
+
+    };
+}
+
+//Метод для подсчета стоимости товаров
+
+
+
+
+let list = new ProductList();
