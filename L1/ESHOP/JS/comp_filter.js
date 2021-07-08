92d99ef7be9fc2d22d468690befe54b807f626ea
mslava1290 <mslava1290@gmail.com>
2021-07-09 01:22:34 +0300

    Починил поиск

--- L1/ESHOP/JS/comp_filter.js
+++ L1/ESHOP/JS/comp_filter.js
@@ -1,12 +1,12 @@
 const filter_el = {
     data () {
         return {
-            userSearch:''
+            userSearch: ''
         }
     },
     template: `<div>
-                    <form action="#" class="search-form" @submit.prevent=$parent.filter>
-                        <input type="text" class="search-field" :v-model=userSearch>
+                    <form action="#" class="search-form" @submit.prevent='$parent.$refs.products.filter(userSearch)'>
+                        <input type="text" class="search-field" v-model='userSearch'>
                         <button class="btn-search" type="submit">Искать</button>
                     </form>
                </div>`,
